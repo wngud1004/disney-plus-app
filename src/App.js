50fc0fdc87608cbe38794e79b718c28703d2a636
wngud1004 <jh1004ben@gmail.com>
2024-12-11 11:20:40 +0900

    카테고리 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import styled from 'styled-components';
 import Nav from './components/Nav'
 import Banner from './components/Banner'
+import Category from './components/Category';
 
 function App() {
   return (
     <Container>
       <Nav/>
       <Banner/>
+      <Category/>
     </Container>
   );
 }
