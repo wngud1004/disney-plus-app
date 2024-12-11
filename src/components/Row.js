6da30ee3e536de7057ba5b1e36629da1f801f3f1
wngud1004 <jh1004ben@gmail.com>
2024-12-11 12:00:55 +0900

    Moive 모달 생성하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import axios from '../api/axios';
 import React, { useCallback, useEffect, useState } from 'react'
 import "./Row.css";
+import MovieModal from './MovieModal';
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([])
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({})
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -15,13 +18,18 @@ const Row = ({ title, id, fetchUrl }) => {
     fetchMovieData();
   }, [fetchMovieData])
 
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie);
+  }
+
   return (
     <div>
       <h2>{title}</h2>
       <div className='slider'>
         <div className='slider__arrow-left'
           onClick={() => {
-            document.getElementById(id).scrollLeft += window.innerWidth - 80
+            document.getElementById(id).scrollLeft -= window.innerWidth - 80
           }}>
           <span className='arrow'>
             {"<"}
@@ -34,11 +42,11 @@ const Row = ({ title, id, fetchUrl }) => {
               className='row__poster'
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
-      </div>
-      <div className='slider__arrow-right'>
+        <div className='slider__arrow-right'>
           <span className='arrow'
           onClick={() => {
             document.getElementById(id).scrollLeft += window.innerWidth - 80
@@ -46,6 +54,14 @@ const Row = ({ title, id, fetchUrl }) => {
             {">"}
           </span>
         </div>
+      </div>
+
+        {modalOpen &&
+        <MovieModal
+          {...movieSelected}
+          setModalOpen={setModalOpen}
+        />
+      }
     </div>
   )
 }
