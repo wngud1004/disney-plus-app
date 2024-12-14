0c8ccf92048a51b68ade384c067b1cdf378a065b
wngud1004 <jh1004ben@gmail.com>
2024-12-14 14:39:40 +0900

    로그인 페이지 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 import SearchPage from './pages/SearchPage';
 import DetailPage from './pages/DetailPage';
 
@@ -24,6 +25,7 @@ function App() {
           {/* 기본 라우트 설정 */}
           <Route path="/" element={<Layout />}>
             {/* 중첩 라우트 */}
+            <Route index element={<LoginPage />} />
             <Route path="main" element={<MainPage />} />
             <Route path="search" element={<SearchPage />} />
             <Route path=":movieId" element={<DetailPage />} />
