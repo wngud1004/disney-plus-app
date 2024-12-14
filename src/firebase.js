35762248f16055d98e92b30f8f06dea5fedc2ee3
wngud1004 <jh1004ben@gmail.com>
2024-12-14 14:55:23 +0900

    firbase key 보안

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVxGeHJjOWVdaf7gMkqQcUoKmNTD3j5Pg",
-  authDomain: "disney-plus-app-a9fa0.firebaseapp.com",
-  projectId: "disney-plus-app-a9fa0",
-  storageBucket: "disney-plus-app-a9fa0.firebasestorage.app",
-  messagingSenderId: "210851188991",
-  appId: "1:210851188991:web:5ae16a2f4703fadd4abcca",
-  measurementId: "G-JMYY35VGLH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
