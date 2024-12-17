420902301b23ae47aa856293469d582b671a2385
wngud1004 <jh1004ben@gmail.com>
2024-12-17 20:08:12 +0900

    이미지 불러옴 오류5

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -6,13 +6,13 @@ const LoginPage = () => {
     <Container>
       <Content>
         <Center>
-          <LogoOne src="/images/cta-logo-one.svg" alt="logo-one" />
+          <LogoOne src="images/cta-logo-one.svg" alt="logo-one" />
           <SignUpLink>지금 가입</SignUpLink>
           <Description>
             영화에 대한 프리미어 액세스를 얻으십시오.
             디즈니 플러스 가격은 다음 주부터 1000원 인상됩니다.
           </Description>
-          <LogoTwo src="/images/cta-logo-two.png" alt="logo-two" />
+          <LogoTwo src="images/cta-logo-two.png" alt="logo-two" />
         </Center>
         <BgImage />
       </Content>
