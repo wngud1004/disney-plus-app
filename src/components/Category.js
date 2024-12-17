4d9b8d6cd362d1f00796874762b7f5c3ee3489d9
wngud1004 <jh1004ben@gmail.com>
2024-12-17 19:55:41 +0900

    이미지와 비디오 불러옴 오류 해결

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,33 +5,33 @@ const Category = () => {
   return (
     <Container>
       <Wrap>
-        <img src="images/viewers-disney.png" alt="disney" />
+        <img src="/images/viewers-disney.png" alt="disney" />
         <video autoPlay loop muted>
-          <source src="videos/disney.mp4" type='video/mp4' />
+          <source src="/videos/disney.mp4" type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="images/viewers-marvel.png" alt="marvel" />
+        <img src="/images/viewers-marvel.png" alt="marvel" />
         <video autoPlay loop muted>
-          <source src="videos/marvel.mp4" type='video/mp4' />
+          <source src="/videos/marvel.mp4" type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="images/viewers-pixar.png" alt="pixar" />
+        <img src="/images/viewers-pixar.png" alt="pixar" />
         <video autoPlay loop muted>
-          <source src="videos/pixar.mp4" type='video/mp4' />
+          <source src="/videos/pixar.mp4" type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="images/viewers-starwars.png" alt="starwars" />
+        <img src="/images/viewers-starwars.png" alt="starwars" />
         <video autoPlay loop muted>
-          <source src="videos/star-wars.mp4" type='video/mp4' />
+          <source src="/videos/star-wars.mp4" type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="images/viewers-national.png" alt="national" />
+        <img src="/images/viewers-national.png" alt="national" />
         <video autoPlay loop muted>
-          <source src="videos/national-geographic.mp4" type='video/mp4' />
+          <source src="/videos/national-geographic.mp4" type='video/mp4' />
         </video>
       </Wrap>
 
