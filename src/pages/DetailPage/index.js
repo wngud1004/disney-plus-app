1ff4dbc3888c82cc77929269fd66b6ee037c74dc
wngud1004 <jh1004ben@gmail.com>
2024-12-19 23:59:16 +0900

    즐겨찾기 버튼 생성성

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -5,6 +5,9 @@ import axios from '../../api/axios';
 const DetailPage = () => {
   let { movieId } = useParams();
   const [movie, setMovie] = useState({});
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem('favorites')) || []
+  );
 
   useEffect(() => {
     async function fetchData() {
@@ -17,6 +20,17 @@ const DetailPage = () => {
     fetchData();
   }, [movieId])
   
+  const handleFavorite = () => {
+    let updatedFavorites = [...favorites];
+    if (updatedFavorites.some((fav) => fav.id === movie.id)) {
+      updatedFavorites = updatedFavorites.filter((fav) => fav.id !== movie.id);
+    } else {
+      updatedFavorites.push(movie);
+    }
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  };
+
   if(!movie) return null;
 
   return (
@@ -26,6 +40,11 @@ const DetailPage = () => {
         src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
         alt="img"
       />
+            <button onClick={handleFavorite}>
+        {favorites.some((fav) => fav.id === movie.id)
+          ? '즐겨찾기 제거'
+          : '즐겨찾기 추가'}
+      </button>
     </section>
   )
 }
