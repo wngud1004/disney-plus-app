b06767f37177b4c8e390c7e829333fa031ee58be
wngud1004 <jh1004ben@gmail.com>
2024-12-22 17:37:33 +0900

    영화 상세 페이지 레이아웃 변경경

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from '../../api/axios';
+import './DetailPage.css';
 
 const DetailPage = () => {
   let { movieId } = useParams();
@@ -34,19 +35,27 @@ const DetailPage = () => {
   if(!movie) return null;
 
   return (
-    <section>
-      <img 
-        className='modal__poster-img'
+    <div className="detail-container">
+      <img
+        className="detail__poster"
         src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-        alt="img"
+        alt={movie.title || movie.name}
       />
-      <button onClick={handleFavorite}>
-        {favorites.some((fav) => fav.id === movie.id)
-          ? '즐겨찾기 제거'
-          : '즐겨찾기 추가'}
-      </button>
-    </section>
-  )
-}
+      <div className="detail__info">
+        <h1 className="detail__title">{movie.title || movie.name}</h1>
+        <p className="detail__tagline">{movie.tagline}</p>
+        <p className="detail__overview">{movie.overview}</p>
+        <p className="detail__details">
+          개봉일: {movie.release_date} | 평점: {movie.vote_average} / 10
+        </p>
+        <button onClick={handleFavorite} className="detail__favorite-btn">
+          {favorites.some((fav) => fav.id === movie.id)
+            ? '즐겨찾기 제거'
+            : '즐겨찾기 추가'}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default DetailPage
