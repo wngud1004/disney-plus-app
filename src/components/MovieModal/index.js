c9855ccce0c173964f642130a81b015da79b7894
wngud1004 <jh1004ben@gmail.com>
2024-12-23 15:34:40 +0900

    영화 상세 페이지 레이아웃 수정정

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react';
 import useOnClickOutside from '../../hooks/useOnClickOutside';
-import "./MovieModal.css";
+import './MovieModal.css';
 
 const MovieModal = ({
   backdrop_path,
@@ -10,19 +10,37 @@ const MovieModal = ({
   release_date,
   first_air_date,
   vote_average,
+  id,
   setModalOpen
 }) => {
-
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem('favorites')) || []
+  );
   const ref = useRef();
 
-  useOnClickOutside(ref, () => { 
+  useOnClickOutside(ref, () => {
     setModalOpen(false);
-  })
-  
+  });
+
+  const handleFavorite = () => {
+    let updatedFavorites = [...favorites];
+    if (updatedFavorites.some((fav) => fav.id === id)) {
+      updatedFavorites = updatedFavorites.filter((fav) => fav.id !== id);
+    } else {
+      updatedFavorites.push({
+        id,
+        title: title || name,
+        backdrop_path
+      });
+    }
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  };
+
   return (
-    <div className='presentation' role="presentation">
-      <div className='wrapper-modal'>
-        <div className='modal' ref={ref}>
+    <div className="presentation" role="presentation">
+      <div className="wrapper-modal">
+        <div className="modal" ref={ref}>
           <span
             onClick={() => setModalOpen(false)}
             className="modal-close"
@@ -31,25 +49,29 @@ const MovieModal = ({
           </span>
 
           <img
-            className='modal__poster-img'
+            className="modal__poster-img"
             src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
-            alt="modal-img"
+            alt={title || name}
           />
 
-          <div className='modal__content'>
-            <p className='modal__details'>
-              <span className='modal__user_perc'>100% for you</span>{" "}
-              {release_date ? release_date : first_air_date}
+          <div className="modal__content">
+            <p className="modal__details">
+              {release_date || first_air_date} | 평점: {vote_average} / 10
             </p>
 
-            <h2 className='modal__title'> {title ? title : name}</h2>
-            <p className='modal__overview'>평점: {vote_average}</p>
-            <p className='modal__overview'>{overview}</p>
+            <h2 className="modal__title">{title || name}</h2>
+            <p className="modal__overview">{overview}</p>
+
+            <button onClick={handleFavorite} className="modal__favorite-btn">
+              {favorites.some((fav) => fav.id === id)
+                ? '즐겨찾기 제거'
+                : '즐겨찾기 추가'}
+            </button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieModal
+export default MovieModal;
